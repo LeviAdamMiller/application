f0803a1dc87be1c19a0b8889b2cc862934ce29a1
Levi Miller <Miller.Levi@student.greenriver.edu>
2024-02-04 15:51:42 -0500

    added personal information stlyes and editied experience page

--- index.php
+++ index.php
@@ -29,5 +29,64 @@ $f3 ->route('GET /personal',function(){
     echo $view->render('views/personalInformation.html');
 
 });
+
+//define default route
+$f3 ->route('GET|POST /experience',function($f3){
+    // display views page
+    $view = new Template();
+    echo $view->render('views/experience.html');
+
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    //validate the data
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $state = $_POST["state"];
+
+
+    // Put the data in the session array
+    $f3->set('SESSION.firstName', $firstName);
+    $f3->set('SESSION.lastName', $lastName);
+    $f3->set('SESSION.email', $email);
+    $f3->set('SESSION.phone', $phone);
+    $f3->set('SESSION.state', $state);
+
+    $f3->reroute('experience');
+    }
+});
+//define default route
+$f3 ->route('GET /JobOpenings',function(){
+    // display views page
+    $view = new Template();
+    echo $view->render('views/jobOpenings.html');
+
+});
+
+
+
+
+//    //If the form has been posted
+//    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+//        // Validate the data
+//        if (isset($_POST['conds'])){
+//            $conds = implode(", ", $_POST['conds']);
+//        }
+//        else {
+//            $conds = "None selected";
+//        }
+//
+//        // Put the data in the session array
+//        $f3->set('SESSION.conds', $conds);
+//
+//        // Redirect to summary route
+//        $f3->reroute('summary');
+//    }
+
+
+
+
 //run fat free
 $f3->run();
